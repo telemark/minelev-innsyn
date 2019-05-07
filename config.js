449e427fa914a745228cb913d5773c7dedbde240
maccyber <jonas.enge@gmail.com>
2019-05-07 14:36:54 +0200

    Adds login promp

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   redirectUri: process.env.VUE_APP_REDIRECT_URL || '<host addr>',
 
   // Login every time
-  extraQueryParameter: 'prompt=login&domain_hint=t-fk.no',
+  extraQueryParameter: 'prompt=login&domain_hint=t-fk.no&login_hint=brukernavn@login.top.no',
 
   // Cache Location
   cacheLocation: 'localStorage',
